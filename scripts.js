e6453202599ea58aebb9e4fbf41bdccc3e7a9212
Michael Connolly <mpc63@uakron.edu>
2023-04-05 14:50:19 -0400

    Add implementation for random avatars

--- scripts.js
+++ scripts.js
@@ -1,29 +1,91 @@
 function generate() {
-  const backgrounds = [];
+  const backgrounds = [
+    'Black.png',
+    'Blue.png',
+    'Green.png',
+    'Grey.png',
+    'Purple.png',
+  ];
+  const props = ['Blood.png', 'Colors.png', 'Leaves.png', 'Numbers.png'];
+  const colors = [
+    'Black.png',
+    'Blue.png',
+    'Green.png',
+    'Red.png',
+    'Yellow.png',
+  ];
+  const eyes = ['Small.png', 'Wide.png'];
+  const pupils = ['Black.png', 'Green.png', 'Purple.png', 'Red.png'];
+  const mouths = [
+    'O-Black.png',
+    'O-Yellow.png',
+    'Sad-Blue.png',
+    'Sad-Purple.png',
+    'Straight-Blue.png',
+  ];
 
   const canvas = document.getElementById('nft');
   const ctx = canvas.getContext('2d');
-  // create new image object
-  const img = new Image();
-  const img2 = new Image();
 
-  // set image source
-  img.src = '/images/Background/Grey.png';
-  img2.src = '/images/Color/Green.png';
+  // create new image objects
+  const backgroundImage = new Image();
+  const propImage = new Image();
+  const colorImage = new Image();
+  const eyeImage = new Image();
+  const pupilImage = new Image();
+  const mouthImage = new Image();
 
-  // wait for image to load before rendering it
-  img.addEventListener('load', function () {
-    ctx.drawImage(img, 0, 0, 640, 360);
+  // Randomly pick each attribute
+  const background =
+    backgrounds[Math.floor(Math.random() * backgrounds.length)];
+  const prop = props[Math.floor(Math.random() * props.length)];
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  const eye = eyes[Math.floor(Math.random() * eyes.length)];
+  const pupil = pupils[Math.floor(Math.random() * pupils.length)];
+  const mouth = mouths[Math.floor(Math.random() * mouths.length)];
+
+  // set image sources
+  const backgroundImagePath = '/images/Background/';
+  const propImagePath = '/images/Props/';
+  const colorImagePath = '/images/Color/';
+  const eyeImagePath = '/images/Eyes/';
+  const pupilImagePath = '/images/Pupils/';
+  const mouthImagePath = '/images/Mouth/';
+
+  backgroundImage.src = backgroundImagePath + background;
+  propImage.src = propImagePath + prop;
+  colorImage.src = colorImagePath + color;
+  eyeImage.src = eyeImagePath + eye;
+  pupilImage.src = pupilImagePath + pupil;
+  mouthImage.src = mouthImagePath + mouth;
+
+  // wait for imagse to load before rendering them
+  backgroundImage.addEventListener('load', function () {
+    ctx.drawImage(backgroundImage, 0, 0, 640, 360);
   });
 
-  img2.addEventListener('load', function () {
-    ctx.drawImage(img2, 0, 0, 640, 360);
+  propImage.addEventListener('load', function () {
+    ctx.drawImage(propImage, 0, 0, 640, 360);
+  });
+  colorImage.addEventListener('load', function () {
+    ctx.drawImage(colorImage, 0, 0, 640, 360);
+  });
+
+  eyeImage.addEventListener('load', function () {
+    ctx.drawImage(eyeImage, 0, 0, 640, 360);
+  });
+  pupilImage.addEventListener('load', function () {
+    ctx.drawImage(pupilImage, 0, 0, 640, 360);
+  });
+
+  mouthImage.addEventListener('load', function () {
+    ctx.drawImage(mouthImage, 0, 0, 640, 360);
   });
 }
 
 function download() {
   const canvas = document.getElementById('nft');
-  const canvasUrl = canvas.toDataURL('/output');
+  const canvasUrl = canvas.toDataURL();
   const createEl = document.createElement('a');
   createEl.href = canvasUrl;
 
